37de458434d8a1367a92c4304dfef99bb53d0763
Curtis Larsen <curtis.gale.larsen@gmail.com>
2019-06-11 11:15:40 -0600

    initial commit of skill

---
+++ README.md
@@ -0,0 +1,15 @@
+## My First Skill
+This skill is a demonstration of a simple skill.  It will respond with
+a simple phrase.
+
+## Description 
+There are several word phrases to activate this skill.  The list
+of possible responses is different depending on activation phrase.
+
+## Example activation phrases
+* "Rocketman"
+* "Guardians of the Galaxy"
+* "What's up"
+
+## Credits 
+[Coding Zendo](https://github.com/fractal13)
---
+++ __init__.py
@@ -0,0 +1,30 @@
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util.log import LOG
+
+__author__ = 'fractal13'
+LOGGER = LOG.create_logger( __name__ )
+
+class MyFirstSkill( MycroftSkill ):
+
+    def __init__( self ):
+        super( MyFirstSkill, self ).__init__( name="MyFirstSkill" )
+        LOGGER.info( "__init__" )
+
+    @intent_handler( IntentBuilder( "" ).require( "RocketmanKeyword" ) )
+    def handle_rocketman_intent( self, message ):
+        LOGGER.info( "rocketman" )
+        self.speak_dialog( "rocketman", data={} )
+
+    @intent_handler( IntentBuilder( "" ).require( "GuardiansKeyword" ).require( "GalaxyKeyword" ) )
+    def handle_guardians_of_the_galaxy_intent( self, message ):
+        LOGGER.info( "" )
+        self.speak_dialog( "guardians.galaxy", data={} )
+
+    @intent_handler( IntentBuilder( "" ).require( "WhatKeyword" ).require( "UpKeyword" ) )
+    def handle_whats_up_intent( self, message ):
+        LOGGER.info( "" )
+        self.speak_dialog( "whats.up", data={} )
+
+def create_skill():
+    return MyFirstSkill()
